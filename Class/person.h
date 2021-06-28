2ed2d4d1c6b71c9788f29f77a396ee5a0cd3848b
shengjie <>
2021-06-28 11:14:06 +0800

    test

--- Class/person.h
+++ Class/person.h
@@ -1,7 +1,10 @@
-#import <UIKit/UIKit.h>
 
-@interface person : UIViewController
+#import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+@interface person : NSObject
 
 @end
 
+NS_ASSUME_NONNULL_END
